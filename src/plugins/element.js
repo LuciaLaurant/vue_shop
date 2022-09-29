e2f2209f970ee18e799e1b85c46742f7e6a45f7f
LuciaXu <3012170145@qq.com>
2022-09-30 01:37:41 +0800

    second push

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import {
+    Message
+} from 'element-ui'
 
 Vue.use(Element)
+Vue.use(Message)
+Vue.prototype.$message = Message
